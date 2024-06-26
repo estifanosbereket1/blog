ebd93493dab4a9a719cab7bddc9309378bc2e04d
Estifanos Bereket <estifanosbereket297@gmail.com>
2024-06-26 14:16:19 +0300

    div added

--- components/CardCategories/CardCategory1.tsx
+++ components/CardCategories/CardCategory1.tsx
@@ -1,71 +1,3 @@
-// import { _getImgRd, _getTagNameRd } from "@/contains/fakeData";
-// import React, { FC } from "react";
-// import NcImage from "@/shared/NcImage/NcImage";
-// import Link from "next/link";
-// import { StaticImageData } from "next/image";
-// import { Category } from "@prisma/client";
-
-// export interface CardCategory1Props {
-//   className?: string;
-//   size?: "large" | "normal";
-//   featuredImage?: string | StaticImageData;
-//   name?: string;
-//   desc?: string;
-//   data: Category[];
-// }
-
-// const CardCategory1: FC<CardCategory1Props> = ({
-//   className = "",
-//   size = "normal",
-//   name,
-//   desc = "",
-//   featuredImage = "",
-//   data,
-// }) => {
-//   // console.log(data);
-
-//   return (
-//     <div>
-//       {data.map((item: Category) => (
-//         <Link
-//           key={item.id}
-//           href={`/blogcat?cat=${item.slug}`}
-//           className={`nc-CardCategory1 flex items-center ${className}`}
-//         >
-//           <NcImage
-//             alt=""
-//             containerClassName={`flex-shrink-0 relative ${
-//               size === "large" ? "w-20 h-20" : "w-12 h-12"
-//             } rounded-lg mr-4 overflow-hidden`}
-//             src={featuredImage || _getImgRd()}
-//             sizes="(max-width: 640px) 100vw, 40vw"
-//             fill
-//           />
-//           <div>
-//             <h2
-//               className={`${
-//                 size === "large" ? "text-lg" : "text-base"
-//               } nc-card-title text-neutral-900 dark:text-neutral-100 font-semibold`}
-//             >
-//               {/* {name || _getTagNameRd()} */}
-//               {item.title}
-//             </h2>
-//             <span
-//               className={`${
-//                 size === "large" ? "text-sm" : "text-xs"
-//               } block mt-[2px] text-neutral-500 dark:text-neutral-400`}
-//             >
-//               {desc}
-//             </span>
-//           </div>
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default CardCategory1;
-
 import { _getImgRd, _getTagNameRd } from "@/contains/fakeData";
 import React, { FC } from "react";
 import NcImage from "@/shared/NcImage/NcImage";
@@ -95,12 +27,11 @@ const CardCategory1: FC<CardCategory1Props> = ({
   return (
     <div>
       {data.map((item: Category) => (
-        <Link
-          key={item.id} // Ensure the key is correctly placed here
-          href={`/blogcat?cat=${item.slug}`}
-          passHref
-        >
-          <a className={`nc-CardCategory1 flex items-center ${className}`}>
+        <div key={item.id}>
+          <Link
+            href={`/blogcat?cat=${item.slug}`}
+            className={`nc-CardCategory1 flex items-center ${className}`}
+          >
             <NcImage
               alt=""
               containerClassName={`flex-shrink-0 relative ${
@@ -127,8 +58,8 @@ const CardCategory1: FC<CardCategory1Props> = ({
                 {desc}
               </span>
             </div>
-          </a>
-        </Link>
+          </Link>
+        </div>
       ))}
     </div>
   );
